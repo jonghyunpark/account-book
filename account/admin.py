181b8ffb002ac31d23518cb5b1143298de551ddb
jonghyunpark <jonghyunpark0@gmail.com>
2017-03-20 21:06:21 +0900

    add transaction_date field to models

--- account/admin.py
+++ account/admin.py
@@ -8,7 +8,10 @@ class AccountAdmin(admin.ModelAdmin):
 class TransactionAdmin(admin.ModelAdmin):
     list_display = ('text', )
 
+class JournalAdmin(admin.ModelAdmin):
+    list_display = ('user_key', 'group', 'debit', 'credit', 'name', 'amount', 'transaction_date')
+
 admin.site.register(Account, AccountAdmin)
 admin.site.register(Transaction, TransactionAdmin)
-admin.site.register(Journal)
+admin.site.register(Journal, JournalAdmin)
 admin.site.register(Tag)
